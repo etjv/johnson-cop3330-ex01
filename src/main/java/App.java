c9cd7adf14e82f891de7223f9d7773c1f25c0c31
etjv <etjv@bellsouth.net>
2021-06-02 22:47:39 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+/*
+Create a program that prompts for your name and prints a greeting using your name.
+
+Example Output
+What is your name? Brian
+Hello, Brian, nice to meet you!
+Constraint
+Keep the input, string concatenation, and output separate.
+Challenges
+Write a new version of the program without using any variables.
+Write a version of the program that displays different greetings for different people.
+Write a version of the program that displays the greeting in a GUI window.
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String userName = in.nextLine();
+        System.out.println("Hello, " + userName + ", nice to meet you!");
+    }
+}
